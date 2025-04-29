57939544236faf2c036d48a7f511fed3e91aea42
mutz555 <mrbosscoc22@gmail.com>
2025-04-30 02:55:03 +0700

    Update Android.mk

--- zygisk/Android.mk
+++ zygisk/Android.mk
@@ -1,10 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := fingerprint_bypass
 LOCAL_SRC_FILES := hook.cpp
-LOCAL_C_INCLUDES += $(TOPDIR)/include
-LOCAL_LDLIBS := -llog
-
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
 include $(BUILD_SHARED_LIBRARY)
